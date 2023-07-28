6bdd8ad85b505dcd3f4d8b19888187dd1cb8289f
AlexArtrip <alex.artrip1999@gmail.com>
2023-07-28 18:47:50 -0500

    Added runon sentence splitting

--- memas/corpus/basic_corpus.py
+++ memas/corpus/basic_corpus.py
@@ -4,7 +4,7 @@ from functools import reduce
 from interface.corpus import Corpus
 from interface.corpus import Citation
 from interface.storage_driver import DocumentEntity
-from memas.storage_driver import corpus_vector_store, corpus_doc_store, corpus_doc_metadata
+from memas.interface.exceptions import SentenceLengthOverflowException
 from memas.context_manager import ctx
 
 class BasicCorpus(Corpus) :
@@ -16,9 +16,14 @@ class BasicCorpus(Corpus) :
     The function stores a document in both the elastic search DB as well as the vecDB.
     Returns True on Success, False on Failure
     """
-    def store_and_index(self, document: str, citation: Citation) -> bool:
+    def store_and_index(self, document: str, document_name: str, citation: Citation) -> bool:
         doc_id = uuid.uuid4()
-        doc_entity = DocumentEntity(self.corpus_ID, doc_id, citation.source_name, document)
+        doc_entity = DocumentEntity(self.corpus_ID, doc_id, document_name, document)
+
+        ctx.corpus_metadata.insert_document_metadata(self.corpus_ID, doc_id, document_name, citation)
+
+        # Divide longer documents for document store
+        segment_document(document)
         return ctx.corpus_doc.save_document(doc_entity) and ctx.corpus_vec.save_document(doc_entity)
 
 
@@ -35,12 +40,8 @@ class BasicCorpus(Corpus) :
         temp_res = ctx.corpus_doc.search_corpus(self.corpus_ID, clue)
         # Search the document store 
         for score, doc_entity in temp_res :
-            # TODO should the basic corpus use the score?
-
             document_text = doc_entity.document
-
-            # TODO : Need to fetch Source URI and description if applicable
-            citation = Citation("source_URI_MISSING", doc_entity.document_name, self.corpus_name, "no description")
+            citation = ctx.corpus_metadata.get_document_citation(self.corpus_ID, doc_entity.document_id)
 
             doc_store_results.append([score, document_text, citation])
             
@@ -50,14 +51,13 @@ class BasicCorpus(Corpus) :
         temp_res2 = ctx.corpus_vec.search(self.corpus_ID, clue) 
         for score, doc_entity, start_index, end_index in temp_res2 :
 
-            # Check that the text recovered from the vectors is full sentences and recovered the rest if needed
+            # Verify that the text recovered from the vectors fits the maximum sentence criteria
             if end_index - start_index != len(doc_entity.document) :
                 # TODO : Create function to fetch the rest of the sentence from the document DB
                 print("Sentence too long and full fetch not implemented")
-                assert False
+                raise SentenceLengthOverflowException(end_index - start_index)
 
-            # TODO : Need to fetch Source URI and description if applicable
-            citation = Citation("source_URI_MISSING", doc_entity.document_name, self.corpus_name, "no description")
+            citation = ctx.corpus_metadata.get_document_citation(self.corpus_ID, doc_entity.document_id)
 
             vec_store_results.append([score, doc_entity.document, citation])
 
@@ -76,11 +76,6 @@ class BasicCorpus(Corpus) :
 
 def normalize_and_combine(doc_results : list, vec_results : list) :
     # normalization with assumption that top score matches are approximately equal
-    print("For Doc we have")
-    print(doc_results)
-
-    print("For vec we have ")
-    print(vec_results)
 
     # Vec scores are based on distance, so smaller is better. Need to inverse the
     # order to be comparable to something like elastic search where bigger is better
@@ -97,12 +92,6 @@ def normalize_and_combine(doc_results : list, vec_results : list) :
     # Normalize and shift all results to be between 0 and 1, with 1 being best responses and 0 being worst  
     doc_results_normalized = [[(x - doc_min_score) / (doc_max_score - doc_min_score) ,y,z] for [x,y,z] in doc_results]
     vec_results_normalized = [[(vec_max_score - x) / (vec_max_score - vec_min_score),y,z] for [x,y,z] in vec_results]
-
-    # print("For Doc scores normalized")
-    # print(doc_results_normalized)
-
-    # print("For vec scores normalized")
-    # print(vec_results_normalized)
 
     # Remove duplicates and merge
     # TODO : replace this with a way to preserve and reward good scoring
